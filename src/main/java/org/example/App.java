dcd6ad63e96dc084a09b2d5f11efa2ec9d109e34
chinnapareddy <nareshreddy2408@gmail.com>
2023-08-25 21:30:43 +0530

    New demo project has been added by naresh

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.demo.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+        Demo demo = new Demo(1,"Prashanta","PH");
+
+        System.out.println(demo);
+    }
+}
---
+++ src/main/java/org/example/demo/Demo.java
@@ -0,0 +1,47 @@
+package org.example.demo;
+
+public class Demo {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Demo(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
